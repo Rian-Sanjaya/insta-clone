138b60dbfeef658c13e6e13247b31c76bfca8e9d
Rian Sanjaya <rian.sj@gmail.com>
2023-12-09 16:01:03 +0700

    create post content

---
+++ src/components/feedPosts/PostFooter.jsx
@@ -0,0 +1,9 @@
+const PostFooter = () => {
+  return (
+    <div>
+      Post Footer
+    </div>
+  )
+}
+
+export default PostFooter
---
+++ src/components/feedPosts/PostHeader.jsx
@@ -0,0 +1,18 @@
+import { Avatar, Box, Flex } from "@chakra-ui/react"
+
+const PostHeader = () => {
+  return (
+    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
+      <Flex alignItems={"center"} gap={2}>
+        <Avatar src="/img1.png" size={"sm"} alt="user profile pic" />
+        <Flex fontSize={12} fontWeight={"bold"} gap={2}>
+          asaprogrammer
+          <Box color={"gray.500"}>∙ 1w</Box>
+        </Flex>
+      </Flex>
+      <Box></Box>
+    </Flex>
+  )
+}
+
+export default PostHeader
