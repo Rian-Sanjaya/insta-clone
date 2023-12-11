dccbc37fadd6d23b8d20b398d686a9100c68ce3b
Rian Sanjaya <rian.sj@gmail.com>
2023-12-11 16:18:26 +0700

    add static post content

--- src/components/feedPosts/PostFooter.jsx
+++ src/components/feedPosts/PostFooter.jsx
@@ -1,8 +1,68 @@
-const PostFooter = () => {
+import { Box, Button, Flex, Input, InputGroup, InputRightElement, Text } from "@chakra-ui/react"
+import { useState } from "react"
+import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../assets/constant"
+
+const PostFooter = ({ username }) => {
+  const [liked, setLiked] = useState(false)
+  const [likes, setLikes] = useState(1000)
+
+  const handleClick = () => {
+    if (liked) {
+      setLiked(false)
+      setLikes(likes - 1)
+    } else {
+      setLiked(true)
+      setLikes(likes + 1)
+    }
+  }
+
   return (
-    <div>
-      Post Footer
-    </div>
+    <Box my={10}>
+      <Flex alignItems={"center"} gap={4} w={"full"} pt={0} mb={2} mt={4}>
+        <Box onClick={handleClick} cursor={"pointer"} fontSize={18}>
+          {
+            !liked ?
+              <NotificationsLogo />
+            : <UnlikeLogo />
+          }
+        </Box>
+
+        <Box cursor={"pointer"} fontSize={18}>
+          <CommentLogo />
+        </Box>
+      </Flex>
+
+      <Text fontSize={"sm"} fontWeight={600}>
+        {likes} likes
+      </Text>
+      <Text fontSize={"sm"} fontWeight={700}>
+        {username} {" "}
+        <Text as={"span"} fontWeight={400}>
+          Feeling good
+        </Text>
+      </Text>
+      <Text fontSize={"sm"} color={"gray"}>
+        View all 1,000 comments
+      </Text>
+
+      <Flex alignItems={"center"} gap={2} justifyContent={"space-between"} w={"full"}>
+          <InputGroup>
+            <Input variant={"flushed"} placeholder="Add a comment..." fontSize={14} />
+            <InputRightElement>
+              <Button
+                fontSize={14}
+                color={"blue.500"}
+                fontWeight={600}
+                cursor={"pointer"}
+                _hover={{ color: "white" }}
+                bg={"transparent"}
+              >
+                Post
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+      </Flex>
+    </Box>
   )
 }
 
--- src/components/feedPosts/PostHeader.jsx
+++ src/components/feedPosts/PostHeader.jsx
@@ -1,16 +1,28 @@
-import { Avatar, Box, Flex } from "@chakra-ui/react"
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react"
 
-const PostHeader = () => {
+const PostHeader = ({ username, avatar }) => {
   return (
     <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"} my={2}>
       <Flex alignItems={"center"} gap={2}>
-        <Avatar src="/img1.png" size={"sm"} alt="user profile pic" />
+        <Avatar src={avatar} size={"sm"} alt={username} />
         <Flex fontSize={12} fontWeight={"bold"} gap={2}>
-          asaprogrammer
+          { username }
           <Box color={"gray.500"}>∙ 1w</Box>
         </Flex>
       </Flex>
-      <Box></Box>
+      <Box cursor={"pointer"}>
+        <Text
+          fontSize={12}
+          color={"blue.500"}
+          fontWeight={"bold"}
+          _hover={{
+            color: "white"
+          }}
+          transition={"0.2s ease-in-out"}
+        >
+          Unfollow
+        </Text>
+      </Box>
     </Flex>
   )
 }
